f89e3478428b57c09fc275b693d77765216efda4
siddhant-glitch <55294144+siddhant-glitch@users.noreply.github.com>
2020-10-16 17:50:15 -0400

    figuring out basic stuff but still incomplete

--- js/main.js
+++ js/main.js
@@ -1,38 +1,40 @@
 (() => {
 
     // make an ajax request using the Fetch API
-    fetch('./data/classData.json')
+    fetch('../Dimri_Siddhant_SassBuild/data/classData.json')
     .then(res => res.json())
     .then(data => {
-            debugger;
+            
         console.log(data);
         //handleData
         //here's where you would call the function that puts the pieces on the page
-        function handleDataSet {
+        function handleDataSet(data) {
             
-            let myData = JSON.parse(data);
             let profPanelText = document.querySelector(".profPanelText");
-            let userTemplate = document.querySelector("#user-template").content;
+            let userTemplate = document.querySelector(".profPanelText").content;
 
-            for (let user in myData) {
-
+            for (let user in data) {
+                
                 let currentUser = userTemplate.cloneNode(true);
+                
                 let currentUserText = currentUser.querySelector('.user').children;
-    
-                currentUserText[1].textContent = data[user].name;
-                currentUserText[2].textContent = data[user].role;
-                currentUserText[3].textContent = data[user].nickname;
-    
-                userSection.appendChild(currentUser);
+                let Primary = currentUser.querySelector('.user').children;
+                text-primary
+                
+                currentUserText[1].innerHTML = data[user].coursename;
+                currentUserText[2].innerHTML = data[user].coursecode;
+                currentUserText[3].innerHTML = data[user].profname;
+                
+                profPanelText.appendChild(currentUser);
             }
-
-            console.log(data);
         }
+
+        handleDataSet(data);
+
     })
+
     .catch((err) => {
         console.log(err);
     })
-
-
 
 })();
